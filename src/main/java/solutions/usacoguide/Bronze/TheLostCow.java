5446a85e0294382a0962909cf7f0f3abceea1cc4
emhgit <eharper0815@gmail.com>
2025-06-19 18:20:49 -0500

    solve the lost cow problem

---
+++ src/main/java/solutions/usacoguide/Bronze/TheLostCow.java
@@ -0,0 +1,53 @@
+package solutions.usacoguide.Bronze;
+
+import java.io.*;
+import java.util.*;
+
+public class TheLostCow {
+    // problem URL: https://usaco.org/index.php?page=viewproblem2&cpid=735
+    public static void main(String[] args) throws IOException {
+
+        boolean LOCAL = true;
+
+        BufferedReader br;
+        PrintWriter out;
+
+        if (LOCAL) {
+            br = new BufferedReader(new FileReader("src/input/lostcow.in"));
+            out = new PrintWriter(new FileWriter("src/output/lostcow.out"));
+        } else {
+            br = new BufferedReader(new FileReader("lostcow.in"));
+            out = new PrintWriter(new FileWriter("lostcow.out"));
+        }
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int dir = 1;
+        int dist = 1;
+        int travelled = 0;
+
+        while (true) {
+            if ((dir == 1 && x <= y && y <= (x + dist * dir)) ||
+                    (dir == -1 && x >= y && y >= (x + dist * dir))) {
+                out.println(travelled + Math.abs(x - y));
+                break;
+            } else {
+                travelled += dist * 2;
+                dist *= 2;
+                dir *= -1;
+            }
+        }
+
+        br.close();
+        out.flush();
+        out.close();
+    }
+    // y
+    // -3 -2 -1 0 1 2 3 4 5 6 7 8 9 10 11
+    // x
+    // dist = 4
+    // travelled = 3
+    // dir = 1
+}

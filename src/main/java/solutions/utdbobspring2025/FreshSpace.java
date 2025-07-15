d5b8282a1b1540a895707c6a506c78a4b5fac39e
emhgit <eharper0815@gmail.com>
2025-07-15 09:12:04 -0500

    create folder for utd bob spring 2025 and solve 2 problems

---
+++ src/main/java/solutions/utdbobspring2025/FreshSpace.java
@@ -0,0 +1,31 @@
+package solutions.utdbobspring2025;
+
+import java.io.*;
+import java.util.*;
+
+public class FreshSpace {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        Set<Integer> visitedRooms = new HashSet<>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int score = Integer.parseInt(st.nextToken());
+            if (!visitedRooms.contains(score)) {
+                out.print(1 + " ");
+                visitedRooms.add(score);
+            } else
+                out.print(0 + " ");
+        }
+
+        br.close();
+        out.flush();
+        out.close();
+    }
+}
---
+++ src/main/java/solutions/utdbobspring2025/LovelyDecryption.java
@@ -0,0 +1,46 @@
+package solutions.utdbobspring2025;
+
+import java.io.*;
+import java.util.*;
+
+public class LovelyDecryption {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+
+        String encryptedMsg = br.readLine();
+        String[] hiddenMsgs = new String[q];
+        for (int i = 0; i < q; i++)
+            hiddenMsgs[i] = br.readLine();
+        StringBuilder sb;
+
+        // iterate 26 times to represent each possible shift
+        outer: for (int i = 1; i <= 25; i++) {
+            sb = new StringBuilder();
+            // perform the shift on the encrypted msg
+            for (int j = 0; j < n; j++) {
+                char cipheredChar = (char) ((encryptedMsg.charAt(j) + i - 'A') % 26 + 'A');
+                sb.append(cipheredChar);
+            }
+            String cipheredStr = sb.toString();
+            // if the shifted string is in the hidden msgs, return that str
+            for (String hiddenMsg : hiddenMsgs) {
+                if (cipheredStr.indexOf(hiddenMsg) != -1) {
+                    out.println(cipheredStr);
+                    break outer;
+                }
+            }
+        }
+
+        br.close();
+        out.flush();
+        out.close();
+    }
+}

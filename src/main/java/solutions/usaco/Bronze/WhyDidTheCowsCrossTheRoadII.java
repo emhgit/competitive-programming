2e582fdc19930cdb9eb7dedd4dee78277328000f
emhgit <eharper0815@gmail.com>
2025-07-24 14:03:37 -0500

    solve why did the cows cross the road II

--- src/main/java/solutions/usaco/Bronze/WhyDidTheCowsCrossTheRoadII.java
+++ src/main/java/solutions/usaco/Bronze/WhyDidTheCowsCrossTheRoadII.java
@@ -1,32 +1,57 @@
 package solutions.usaco.Bronze;
 
 import java.io.*;
-import java.util.*;
 
 public class WhyDidTheCowsCrossTheRoadII {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter out = new PrintWriter(System.out);
         String paths = br.readLine();
-        Stack<Character> visited = new Stack<>();
-        for (int i = 0; i < paths.length(); i++) {
-            if (!visited.isEmpty()) {
-                if (visited.contains(paths.charAt(i))) {
-                    while (!visited.isEmpty() && visited.peek() != paths.charAt(i))
-                        visited.pop();
-                    if (!visited.isEmpty() && visited.peek() == paths.charAt(i))
-                        visited.pop();
-                } else {
-                    visited.push(paths.charAt(i));
+        Letter[] letters = new Letter[26];
+        for (int i = 0; i < 26; i++) {
+            char ch = (char) ('A' + i);
+            int start = -1;
+            int end = -1;
+            for (int j = 0; j < 52; j++) {
+                if (paths.charAt(j) != ch)
+                    continue;
+                if (start == -1)
+                    start = j;
+                else {
+                    end = j;
+                    break;
                 }
-            } else {
-                visited.push(paths.charAt(i));
+            }
+            letters[i] = new Letter(ch, start, end);
+        }
+
+        int crossingPairs = 0;
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 26; j++) {
+                if (letters[j].ch == letters[i].ch)
+                    continue;
+                if (letters[i].start < letters[j].start && letters[i].end < letters[j].end &&
+                        letters[j].start < letters[i].end) {
+                    crossingPairs++;
+                }
             }
         }
-        out.println(visited.size());
+        out.println(crossingPairs);
 
         br.close();
         out.flush();
         out.close();
+    }
+
+    static class Letter {
+        char ch;
+        int start;
+        int end;
+
+        public Letter(char ch, int start, int end) {
+            this.ch = ch;
+            this.start = start;
+            this.end = end;
+        }
     }
 }
